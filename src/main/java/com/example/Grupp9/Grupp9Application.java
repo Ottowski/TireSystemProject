acb3802e6fe5bf69cb0d93b68f9bba59eef4213b
Ottowski <otto.arvidsson94@gmail.com>
2023-11-21 11:18:39 +0100

    first commit

---
+++ src/main/java/com/example/Grupp9/Grupp9Application.java
@@ -0,0 +1,13 @@
+package com.example.Grupp9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp9Application.class, args);
+	}
+
+}
