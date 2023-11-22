48d9d22b3ff08832add4836ae9c3fa259cd42770
KilRoy95 <Leopwn@hotmail.com>
2023-11-22 13:21:56 +0100

    added partial tyre logic

--- src/main/java/com/example/Grupp9/controller/TyreController.java
+++ src/main/java/com/example/Grupp9/controller/TyreController.java
@@ -3,7 +3,9 @@ package com.example.Grupp9.controller;
 import com.example.Grupp9.model.Tyre;
 import com.example.Grupp9.service.TyreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,10 @@ public class TyreController {
     @GetMapping("/tyres")
     public List<Tyre> getAllTyres() {
         return tyreService.getAllTyres();
+    }
+
+    @PostMapping("/tyres")
+    public ResponseEntity<String> newTyre(Tyre tyre, String type) {
+        return tyreService.newTyre(tyre, type);
     }
 }
--- src/main/java/com/example/Grupp9/model/Tyre.java
+++ src/main/java/com/example/Grupp9/model/Tyre.java
@@ -1,9 +1,6 @@
 package com.example.Grupp9.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity(name = "tyre")
@@ -18,8 +15,7 @@ public class Tyre {
     public Tyre() {
     }
 
-    public Tyre(Long id, String name, String type) {
-        this.id = id;
+    public Tyre(String type) {
         this.type = type;
     }
 }
