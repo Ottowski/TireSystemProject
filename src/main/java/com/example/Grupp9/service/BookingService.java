660833508dfd1a1f5b0d4af50a5a2b05b8e61a8d
KilRoy95 <Leopwn@hotmail.com>
2023-11-29 16:01:24 +0100

    Added newBooking logic
    new Bookings now subtracts from tyres.
    (Booking.amount - Tyre.amount)

--- src/main/java/com/example/Grupp9/service/BookingService.java
+++ src/main/java/com/example/Grupp9/service/BookingService.java
@@ -1,19 +1,44 @@
 package com.example.Grupp9.service;
 
 import com.example.Grupp9.model.Booking;
+import com.example.Grupp9.model.Tyre;
 import com.example.Grupp9.repository.BookingRepository;
+import com.example.Grupp9.repository.TyreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingService {
+    private final BookingRepository bookingRepository;
+    private final TyreRepository tyreRepository;
+
     @Autowired
-    private BookingRepository bookingRepository;
+    public BookingService(BookingRepository bookingRepository, TyreRepository tyreRepository) {
+        this.bookingRepository = bookingRepository;
+        this.tyreRepository = tyreRepository;
+    }
 
     public Booking newBooking(Booking booking) {
-        return bookingRepository.save(booking);
+        Optional<Tyre> tyreOptional = tyreRepository.findByType(booking.getType());
+
+        if (tyreOptional.isPresent()) {
+            Tyre tyre = tyreOptional.get();
+            int remainingTyreAmount = tyre.getAmount() - booking.getAmount();
+
+            if (remainingTyreAmount >= 0) {
+                // If enough tyres, update tyre amount and save booking
+                tyre.setAmount(remainingTyreAmount);
+                tyreRepository.save(tyre);
+                return bookingRepository.save(booking);
+            } else {
+                throw new RuntimeException("Not enough tyres available for booking");
+            }
+        } else {
+            throw new RuntimeException("Tyre not found for booking type: " + booking.getType());
+        }
     }
 
     public List<Booking> findAllBookings() {
